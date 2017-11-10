2781c89739d39daf4fd315495a7c1ff5954bfec8
Bob <bob.taffet@gmail.com>
2017-11-09 20:59:43 -0500

    completed lecture #42

--- themes/whatsamata/single-professor.php
+++ themes/whatsamata/single-professor.php
@@ -1,48 +1,50 @@
 <?php
 
-	get_header();
-	
-	while(have_posts()) {
-		the_post(); ?>
-		  <div class="page-banner">
-            <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner'] ?>);"></div>
-              <div class="page-banner__content container container--narrow">
-                <h1 class="page-banner__title"><?php the_title(); ?></h1>
-                <div class="page-banner__intro">
-                  <p><?php the_field('page_banner_subtitle') ?></p>
-                </div>
-              </div>
-            </div>
+  get_header();
 
-            <div class="container container--narrow page-section">
-              <div class="generic-content">
-                <div class="row group">
-                    <div class="one-third">
-                        <?php the_post_thumbnail('professorPortrait'); ?>
-                    </div>
-                    <div class="two-thirds">
-                        <?php the_content(); ?>
-                    </div>
+  while(have_posts()) {
+    the_post();
+    pageBanner();
+    ?>
 
-                </div>
-              </div>
-              <?php
-                $relatedPrograms = get_field('related_programs');
-                if($relatedPrograms) {
-                    echo '<ul class="link-list min-list">';
-                    echo '<hr class="section-break">';
-                    echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
+    <div class="container container--narrow page-section">
 
-                    foreach($relatedPrograms as $program) { ?>
-                      <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
-                    <?php }
-                    echo '</ul>';
-                } ?>
-            </div>
+      <div class="generic-content">
+        <div class="row group">
 
-	<?php 
-	}
-	
-	
-	get_footer();
+          <div class="one-third">
+            <?php the_post_thumbnail('professorPortrait'); ?>
+          </div>
+
+          <div class="two-thirds">
+            <?php the_content(); ?>
+          </div>
+
+        </div>
+      </div>
+
+      <?php
+
+        $relatedPrograms = get_field('related_programs');
+
+        if ($relatedPrograms) {
+          echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
+          echo '<ul class="link-list min-list">';
+          foreach($relatedPrograms as $program) { ?>
+            <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+          <?php }
+          echo '</ul>';
+        }
+
+      ?>
+
+  </div><!-- class="page-banner" is declared in pageBanner() in functions.php -->
+
+
+
+  <?php }
+
+  get_footer();
+
 ?>
