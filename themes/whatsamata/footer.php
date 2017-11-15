aded2511221bf6313da078667edc2eee2b4b1031
Bob <bob.taffet@gmail.com>
2017-11-15 03:48:53 -0500

    completed lecture 52

--- themes/whatsamata/footer.php
+++ themes/whatsamata/footer.php
@@ -1,4 +1,4 @@
-  <footer class="site-footer">
+<footer class="site-footer">
 
     <div class="site-footer__inner container container--narrow">
 
@@ -50,6 +50,17 @@
 
     </div>
   </footer>
-  <?php wp_footer();?>
-  </body>
+
+  <div class="search-overlay">
+    <div class="search-overlay__top">
+      <div class="container">
+        <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+        <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+        <i class="fa fa-close search-overlay__close" aria-hidden="true"></i>
+      </div>
+    </div>
+  </div>
+
+<?php wp_footer(); ?>
+</body>
 </html>
