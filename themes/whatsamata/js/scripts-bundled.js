c35238c935b006ba2db1a07719b0ba1ee44acc56
Bob <bob.taffet@gmail.com>
2017-11-16 16:02:33 -0500

    completed lecture 56

--- themes/whatsamata/js/scripts-bundled.js
+++ themes/whatsamata/js/scripts-bundled.js
@@ -10611,10 +10611,11 @@ var Search = function () {
     value: function getResults() {
       var _this = this;
 
-      _jquery2.default.getJSON('http://watsamata-u.dev/wp-json/wp/v2/posts?search=' + this.searchField.val(), function (posts) {
-        _this.resultsDiv.html("\n        <h2 class=\"search-overlay__section-title\">General Information</h2>\n        <ul class=\"link-list min-list\">\n          " + posts.map(function (item) {
+      _jquery2.default.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), function (posts) {
+        _this.resultsDiv.html("\n        <h2 class=\"search-overlay__section-title\">General Information</h2>\n        " + (posts.length ? '<ul class="link-list min-list">' : 'No general information available for this search topic') + "\n          " + posts.map(function (item) {
           return "<li><a href=\"" + item.link + "\">" + item.title.rendered + "</a></li>";
-        }).join('') + "\n        </ul>\n      ");
+        }).join('') + "\n        " + (posts.length ? '</ul>' : '') + "\n      ");
+        _this.isSpinnerVisible = false;
       });
     }
   }, {
