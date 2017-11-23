2829160e2b922ab7ae69634d64a990583b8a75c3
Bob <bob.taffet@gmail.com>
2017-11-22 23:31:29 -0500

    completed lecture 69

--- themes/whatsamata/inc/search-route.php
+++ themes/whatsamata/inc/search-route.php
@@ -48,7 +48,8 @@ function universitySearchResults($data) {
     if (get_post_type() == 'program') {
       array_push($results['programs'], array(
         'title' => get_the_title(),
-        'permalink' => get_the_permalink()
+        'permalink' => get_the_permalink(),
+        'id' => get_the_id()
       ));
     }
 
@@ -67,14 +68,51 @@ function universitySearchResults($data) {
       } else {
         $description = wp_trim_words(get_the_content(), 18);
       }
+
       array_push($results['events'], array(
         'title' => get_the_title(),
         'permalink' => get_the_permalink(),
         'month' => $eventDate->format('M'),
-        'day' =>  $eventDate->format('d'),
+        'day' => $eventDate->format('d'),
         'description' => $description
       ));
     }
+
+  }     // end while
+
+  if ($results['programs']) {
+    $programsMetaQuery = array('relation' => 'OR');
+
+    foreach($results['programs'] as $item) {
+      array_push($programsMetaQuery, array(
+          'key' => 'related_programs',
+          'compare' => 'LIKE',
+          'value' => '"' . $item['id'] . '"'
+        ));
+    }
+
+    $programRelationshipQuery = new WP_Query(array(
+      'post_type' => 'professor',
+      'meta_query' => $programsMetaQuery
+    ));
+
+    while($programRelationshipQuery->have_posts()) {
+      $programRelationshipQuery->the_post();
+
+      if (get_post_type() == 'professor') {
+        array_push($results['professors'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink(),
+          'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+        ));
+      }
+
+    }     // end while
+
+    $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
   }
+
+
   return $results;
+
 }
